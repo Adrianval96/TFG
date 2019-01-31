e390e16ad9af0b1b35120300a64157b93bd17046
adrianval96 <adrianval96@gmail.com>
2019-01-31 18:00:25 +0100

    Updated shell test script

--- workdir/rigel/executables/test.sh
+++ workdir/rigel/executables/test.sh
@@ -5,7 +5,7 @@
 #$ -l h_vmem=4g
 #$ -l h_rt=00:30:00
 
-source activate openai
+source /home/advagi/anaconda3/envs/openai/bin/activate
 
 cd /home/advagi/TFG_OpenAI/workdir/rigel/executables/python_scripts
 /home/advagi/anaconda3/envs/openai/bin/python test.py

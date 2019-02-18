d69b7ccd135ba141b57c1a5a941ae363205a764b
adrianval96 <adrianval96@gmail.com>
2019-02-18 17:49:19 +0100

    Tests

--- workdir/scripts/rigel/test.sh
+++ workdir/scripts/rigel/test.sh
@@ -8,4 +8,7 @@
 source /home/advagi/anaconda3/envs/openai/bin/activate
 
 cd /home/advagi/TFG_OpenAI/workdir/scripts/rigel/python_scripts
+
+conda list 
+
 /home/advagi/anaconda3/envs/openai/bin/python test.py
